6798eb322d6a12dba948aefb076daffc4ecb3de6
Ian Landel <iylandel@gmail.com>
2020-01-07 10:09:19 +1000

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
+
 import Layout from "../components/Layout"
+import Banner from "../components/Banner"
+
+import styles from "../css/error.module.css"
 
 const errorPage = () => {
   return (
     <Layout>
-      <h2>opps dead end</h2>
-      <Link to="/">return home</Link>
+      <header className={styles.error}>
+        <Banner title="something has gone wrong">
+          <Link to="/" className="btn-white">
+            return to home page
+          </Link>
+        </Banner>
+      </header>
     </Layout>
   )
 }
